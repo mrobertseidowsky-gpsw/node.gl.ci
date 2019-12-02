d19d7c7ac9c9f5c92f110c7828c861d3d9879793
Clément Bœsch <cboesch@gopro.com>
2019-12-02 12:28:13 +0100

    preview

--- tl.py
+++ tl.py
@@ -4,6 +4,82 @@
 import Tkinter as tk
 import ttk
 
+import pynodegl as ngl
+from pynodegl_utils.misc import Media
+
+
+class _NGLWidget(tk.Canvas):
+
+    def __init__(self, master, filename):
+        tk.Canvas.__init__(self, master)
+        self._viewer = ngl.Viewer()
+        self._time = 0
+        self._media = Media(filename)
+        self.bind("<Configure>", self._configure)
+        self.bind("<Expose>", self._draw)
+        #self.bind("<Activate>", self._draw)
+        #self.bind("<Deactivate>", self._draw)
+        #self.bind("<Enter>", self._draw)
+        #self.bind("<Leave>", self._draw)
+        #self.bind("<Visibility>", self._draw)
+        #self.bind("<FocusIn>", self._draw)
+        #self.bind("<FocusOut>", self._draw)
+        #self.bind("<ResizeRequest>", self._draw)
+
+    @staticmethod
+    def get_viewport(width, height, aspect_ratio):
+        view_width = width
+        view_height = width * aspect_ratio[1] / aspect_ratio[0]
+        if view_height > height:
+            view_height = height
+            view_width = height * aspect_ratio[0] / aspect_ratio[1]
+        view_x = (width - view_width) // 2
+        view_y = (height - view_height) // 2
+        return (view_x, view_y, view_width, view_height)
+
+    @staticmethod
+    def _get_scene(media):
+        #m0 = cfg.medias[0]
+        #cfg.duration = m0.duration
+        #cfg.aspect_ratio = (m0.width, m0.height)
+
+        q = ngl.Quad((-1, -1, 0), (2, 0, 0), (0, 2, 0))
+        m = ngl.Media(media.filename)
+        t = ngl.Texture2D(data_src=m)
+        render = ngl.Render(q)
+        render.update_textures(tex0=t)
+        return render
+
+    def _configure(self, event):
+        print 'configure', event
+
+        #self.wait_visibility()
+
+        self._wid = self.winfo_id()
+        self._width = self.winfo_width()
+        self._height = self.winfo_height()
+        self._aspect_ratio = self._media.dimensions
+
+        self._viewer.configure(window=self._wid,
+                         width=self._width,
+                         height=self._height,
+                         viewport=self.get_viewport(self._width, self._height, self._aspect_ratio),
+                         swap_interval=1)
+        scene = self._get_scene(self._media)
+        self._viewer.set_scene(scene)
+        self._refresh()
+
+    def _refresh(self):
+        self._viewer.draw(self._time)
+
+    def _draw(self, event):
+        print 'draw', event.type
+        self._refresh()
+
+    def seek(self, value):
+        self._time = float(value)
+        self._refresh()
+
 
 class _Segment:
 
@@ -234,9 +310,21 @@ class _Buttons:
 
 
 def  _main():
-    w = tk.Tk()
+    import sys
 
-    z = _Z(w)
+    w = tk.Tk()
+    w.title('Trim view')
+
+    style = ttk.Style()
+    style.theme_use('clam')
+
+    nglw = _NGLWidget(w, sys.argv[1])
+    nglw.pack(fill=tk.BOTH, expand=tk.YES)
+
+    seekbar = ttk.Scale(w, from_=0, to=100, command=nglw.seek, orient=tk.HORIZONTAL)
+    seekbar.pack(fill=tk.X)
+
+    z = _Z(w, nglw)
     buttons = _Buttons(w, z)
 
     z.pack(fill=tk.BOTH, expand=True)
