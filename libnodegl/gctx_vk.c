2a31c72d38dc3bad5d784ae5e40819be42561306
Matthieu Bouron <matthieu.bouron@gmail.com>
2019-12-11 10:50:36 +0100

    missing files

---
+++ libnodegl/gctx_vk.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2019 GoPro Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <string.h>
+
+#include "gctx.h"
+
+void ngli_gctx_set_rendertarget(struct ngl_ctx *s, struct rendertarget *rt)
+{
+}
+
+struct rendertarget *ngli_gctx_get_rendertarget(struct ngl_ctx *s)
+{
+}
+
+void ngli_gctx_set_viewport(struct ngl_ctx *s, const int *viewport)
+{
+}
+
+void ngli_gctx_get_viewport(struct ngl_ctx *s, int *viewport)
+{
+}
+
+void ngli_gctx_set_scissor(struct ngl_ctx *s, const int *scissor)
+{
+}
+
+void ngli_gctx_set_clear_color(struct ngl_ctx *s, const float *color)
+{
+}
+
+void ngli_gctx_get_clear_color(struct ngl_ctx *s, float *color)
+{
+}
+
+void ngli_gctx_clear_color(struct ngl_ctx *s)
+{
+}
+
+void ngli_gctx_clear_depth_stencil(struct ngl_ctx *s)
+{
+}
+
+void ngli_gctx_invalidate_depth_stencil(struct ngl_ctx *s)
+{
+}
---
+++ libnodegl/rendertarget_vk.c
@@ -0,0 +1,18 @@
+#include "rendertarget.h"
+
+int ngli_rendertarget_init(struct rendertarget *s, struct ngl_ctx *ctx, const struct rendertarget_params *params)
+{
+    return 0;
+}
+
+void ngli_rendertarget_blit(struct rendertarget *s, struct rendertarget *dst, int vflip)
+{
+}
+
+void ngli_rendertarget_read_pixels(struct rendertarget *s, uint8_t *data)
+{
+}
+
+void ngli_rendertarget_reset(struct rendertarget *s)
+{
+}
