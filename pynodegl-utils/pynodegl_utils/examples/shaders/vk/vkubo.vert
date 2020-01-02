737ab10fa1c3679930659000ddc8dd42a09cde73
Matthieu Bouron <matthieu.bouron@gmail.com>
2020-01-02 13:30:15 +0100

    XXXX

---
+++ pynodegl-utils/pynodegl_utils/examples/shaders/vk/vkubo.vert
@@ -0,0 +1,31 @@
+#version 450
+#extension GL_ARB_separate_shader_objects : enable
+
+out gl_PerVertex {
+    vec4 gl_Position;
+};
+
+layout(location = 0) in vec3 ngl_position;
+
+layout(push_constant) uniform ngl_block {
+    mat4 modelview_matrix;
+    mat4 projection_matrix;
+} ngl;
+
+layout(binding = 0) uniform VertexParameter {
+    vec4  color0;
+    vec4  color1;
+    vec4  color2;
+} parameters;
+
+layout(binding = 0) buffer ZOB {
+    vec4 color0;
+} coucou;
+
+layout(location = 0) out vec4 color;
+
+void main()
+{
+    gl_Position = ngl.projection_matrix * ngl.modelview_matrix * vec4(ngl_position, 1.0);
+    color = parameters.color2 + coucou.color0;
+}
--- pynodegl-utils/pynodegl_utils/examples/vulkan.py
+++ pynodegl-utils/pynodegl_utils/examples/vulkan.py
@@ -65,6 +65,26 @@ def vkuniform(cfg, color2=(1.0, 0.0, 1.0, 1.0), factor0=1.0, factor1=1.0):
     return render
 
 
+@scene(color2=scene.Color(),
+       factor0=scene.Range(range=[0,1], unit_base=100),
+       factor1=scene.Range(range=[0,1], unit_base=100))
+def vkubo(cfg, color0=(1.0, 0.0, 1.0, 1.0), factor0=1.0, factor1=1.0):
+
+    block = ngl.Block(fields=[
+            ngl.UniformFloat(value=factor0),
+            ngl.UniformFloat(value=factor1)
+            ngl.UniformVec4(value=color0),
+            ], layout='std140')
+
+    geometry = ngl.Quad()
+    program = ngl.Program(fragment=cfg.get_frag('vkubo'),
+                          vertex=cfg.get_vert('vkubo'))
+    render = ngl.Render(geometry, program)
+
+    render.update_blocks(block=block)
+    return render
+
+
 @scene()
 def vktexture_media(cfg):
     group = ngl.Group()
